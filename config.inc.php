7f0202f8e3967cc7791ee0ab63b0e8aca9cd2c64
phl0 <florian@florian-wolters.de>
2016-10-23 16:27:24 +0200

    Some annotation to the config file

--- config.inc.php
+++ config.inc.php
@@ -1,4 +1,10 @@
 <?php
+// This is the IP-address of the Cisco router interfacing the Raspberry
+// Pi that runs the P25NX stuff
 $host = "172.31.5.13";
+
+// This is the port that is routed to the AUX port of the Cisco router
+// The Quantar is connected to the AUX port using a custom made cable 
+// with pinout from http://p25.io/access-your-quantar-via-cisco/
 $port = 2065;
 ?>
